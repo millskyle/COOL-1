bc9e97a4f18c75b49c8b9ed57777001aad746368
Kyle Mills <kyle.mills@uoit.net>
2020-05-15 15:52:27 -0400

    updated setup with Anaconda paths

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup, Extension
 import os
 
 
-tclap_include="/usr/include/tclap"
-boost_lib=""
-boost_include=""
+tclap_include=os.environ['HOME'] + "/anaconda3/pkgs/tclap-1.2.1-h470a237_1/include/"
+boost_lib=os.environ['HOME'] + "/anaconda3/pkgs/libboost-1.67.0-h46d08c1_4/lib"
+boost_include=os.environ['HOME'] + "/anaconda3/pkgs/libboost-1.67.0-h46d08c1_4/include"
 
 import numpy as np
 try:
@@ -19,7 +19,7 @@ extra_compile_args += ["-fopenmp"]
 sa_module = Extension('_sa_interface',
                       language='c++',
                       include_dirs=[tclap_include,boost_include, '/usr/local/opt/llvm/include',numpy_include],
-                      library_dirs=[boost_lib,'/usr/local/opt/llvm/lib',],
+                      library_dirs=[boost_lib,'/usr/local/opt/llvm/lib'],
                       sources=['sagym/interface/sa_interface_wrap.cxx', 
                         'sagym/interface/sa.cc'],
                       extra_compile_args=extra_compile_args,
